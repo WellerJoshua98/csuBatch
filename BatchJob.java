3cf0dce53e15ffbcbdf51ad1285f6c5ee66a8fa4
Joshua Weller <wellerjoshua99@gmail.com>
2025-02-23 19:48:46 -0500

    Updated Batch Job file

--- BatchJob.java
+++ BatchJob.java
@@ -1,8 +1,36 @@
 public class BatchJob {
     private String jobName;
+    private int executionTime;
+    private int priority;
 
-    public BatchJob(String jobName){
+    public BatchJob(String jobName, int executionTime, int priority) {
         this.jobName = jobName;
+        this.executionTime = executionTime;
+        this.priority = priority;
+    }
+
+    public String getJobName() {
+        return jobName;
+    }
+
+    public void setJobName(String jobName) {
+        this.jobName = jobName;
+    }
+
+    public int getExecutionTime() {
+        return executionTime;
+    }
+
+    public void setExecutionTime(int executionTime) {
+        this.executionTime = executionTime;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
     }
 
     public void execute(){
